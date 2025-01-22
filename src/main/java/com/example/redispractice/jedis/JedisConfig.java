97ad890cc11bcd32d7ec36f355d2bfd933736967
Sepehr Jedari <Sepehristaken@gmail.com>
2025-01-22 11:34:49 +0330

    Update redisConfig

--- src/main/java/com/example/redispractice/jedis/JedisConfig.java
+++ src/main/java/com/example/redispractice/jedis/JedisConfig.java
@@ -1,8 +1,11 @@
 package com.example.redispractice.jedis;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
@@ -10,16 +13,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class JedisConfig {
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localHost", 6379);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
-    RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
+    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
 }
